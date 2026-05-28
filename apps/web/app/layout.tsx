d4957d7142735697de0ff4eb15edd733f4afe9df
rayjaywolf <jintarehaan@gmail.com>
2026-05-28 21:04:25 +0530

    analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 import { Providers } from "@/components/providers";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
     >
       <body className="flex min-h-full flex-col bg-background text-foreground">
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
